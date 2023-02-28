4ec884338820da2fc4849866fe484ac4e0389206
oton1 <otoniotoni22@gmail.com>
2023-02-28 11:09:36 -0300

    Alterações agressivas na lógica do código pra ler .csv, usei um df diferente pra analisar dessa vez, output funcionando plenamente

--- analise_texto/main.py
+++ analise_texto/main.py
@@ -1,36 +1,34 @@
-import nltk
+import pandas as pd 
+from deep_translator import GoogleTranslator
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.sentiment import SentimentIntensityAnalyzer
-from deep_translator import GoogleTranslator
 import re
-import pandas as pd 
+import nltk
+import json
 
 nltk.download('stopwords')
-sia = SentimentIntensityAnalyzer()
 
 class sentimental_text:
-
-    def __init__(self,language='english',translate=False):
+    
+    def __init__(self, language='english', translate=False):
         self.language = language
         self.translate = translate
-            
-
-    def pre_processamento(self,frase):
-
+        self.sia = SentimentIntensityAnalyzer()
+    
+    def pre_processamento(self, frase):
         '''
         Retorna o sentimento negativo/positivo de uma frase
-
+        
                 Parameters:
                         frase (str): Frase a ser analisada
-
+        
                 Returns:
                         result (float): -1 -> Negativo / 0 -> Neutro / 1 -> Positivo
         '''
-
         if self.translate:
             frase = GoogleTranslator(source='auto', target=self.language).translate(frase)
-
+        
         stop_words = set(stopwords.words(self.language))
         emoji_pattern = re.compile("["
                                     u"\U0001F600-\U0001F64F"  # emoticons
@@ -43,27 +41,23 @@ class sentimental_text:
         filtered_sentence = [word for word in word_tokens if word.isalnum() and word not in stop_words]
         return ' '.join(filtered_sentence)
 
-    def sentimento(self,frase=''):
-        
+    def sentimento(self, frase=''):
         '''
         Retorna um decimal -1 a 1 
-
+        
                 Parameters:
                         frase (str): Frase a ser analisada
-
+        
                 Returns:
                         result (str): Binary string of the sum of a and b
         '''
-
         preprocessed_sentence = self.pre_processamento(frase)
-        sentiment_score = sia.polarity_scores(preprocessed_sentence)
+        sentiment_score = self.sia.polarity_scores(preprocessed_sentence)
         return sentiment_score['compound']
 
 if __name__ == '__main__':
-
-    comments = ["Tenho muito medo disso", "Deus nos abandonou", "Na prática, eu gostei muito dessa ideia", "Eu não sei o que sentir a respeito desse tema."]
-    s_text = sentimental_text(language='english',translate=True) 
-
-    for comment in comments:
-        sentiment_score = s_text.sentimento(comment)
-        print(sentiment_score)
+    df = pd.read_csv('review.csv')
+    df['text'] = df['title'] + ' ' + df['body_text']
+    df['sentiment_score'] = df['text'].apply(lambda x: sentimental_text(language='english', translate=True).sentimento(x))
+    df.to_csv('comments_with_sentiment_scores.csv', index=False)
+    print(df)
