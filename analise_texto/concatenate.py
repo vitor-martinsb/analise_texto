d845c9991434dd1e359e4a44c35c8097fd0977d5
oton1 <otoniotoni22@gmail.com>
2023-03-13 14:20:51 -0300

    Retorno pra lógica original de leitura e input do sentiment score

--- analise_texto/concatenate.py
+++ analise_texto/concatenate.py
@@ -1,22 +1,13 @@
 import pandas as pd
 
-if __name__ == '__main__':
-    df = pd.read_csv('dataset.csv')
-    comment_cols = [col for col in df.columns if col.startswith('comments')]
-    comments_df = df.melt(value_vars=comment_cols, var_name='comment_num', value_name='comment')
-    comments_df = comments_df.dropna()
-    comments_df = comments_df.reset_index(drop=True)
+# Load the CSV file
+df = pd.read_csv('saidarefor.csv', sep=';', header=None, names=['comments'], engine='python')
 
-    # Concatenate comments by index
-    print("Concatenating comments by index...")
-    comments_df = comments_df.groupby(comments_df['comment_num'].astype(str).str.split('/').str[0]).apply(lambda x: ','.join(x['comment']))
+# Split the "comments" column into multiple rows
+df = df.assign(comments=df['comments'].str.split(',')).explode('comments')
 
-    # Create new DataFrame with just one column 'comments'
-    print("Creating new DataFrame with just one column 'comments'...")
-    comments_df = pd.DataFrame({'comments': comments_df})
+# Reset the index of the DataFrame
+df = df.reset_index(drop=True)
 
-    # Write to CSV file
-    print("Writing to CSV file...")
-    comments_df.to_csv('output.csv', index=False)
-    
-    print("Done!")
+# Save the modified DataFrame to a new CSV file
+df.to_csv('new_file.csv', index=False)
--- analise_texto/index.py
+++ analise_texto/index.py
@@ -1,96 +1,69 @@
-import pandas as pd
+import pandas as pd 
+import nltk
 from deep_translator import GoogleTranslator
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.sentiment import SentimentIntensityAnalyzer
-import re
-import nltk
-from collections import Counter
 
 nltk.download('stopwords')
 
-class sentimental_text:
+class SentimentalText:
+    """
+    A class that preprocesses text and calculates its sentiment score using the
+    SentimentIntensityAnalyzer class from the NLTK library.
+    """
     def __init__(self, language='english', translate=False):
+        """
+        Initializes the SentimentalText object.
+
+        Parameters:
+            language (str): The language of the text to be analyzed. Defaults to 'english'.
+            translate (bool): A boolean indicating whether the text should be translated to the
+            specified language using the GoogleTranslator class from the deep_translator package.
+            Defaults to False.
+        """
         self.language = language
         self.translate = translate
         self.sia = SentimentIntensityAnalyzer()
-    
+
     def pre_processamento(self, frase):
-        '''
-        Retorna o sentimento negativo/positivo de uma frase
-        
-                Parameters:
-                        frase (str): Frase a ser analisada
-        
-                Returns:
-                        result (float): -1 -> Negativo / 0 -> Neutro / 1 -> Positivo
-        '''
+        """
+        Preprocesses a text by removing stop words, tokenizing it, and converting all words to
+        lowercase.
+
+        Parameters:
+            frase (str): The text to be preprocessed.
+
+        Returns:
+            A string with the preprocessed text.
+        """
         if self.translate:
             frase = GoogleTranslator(source='auto', target=self.language).translate(frase)
-        
-        stop_words = set(stopwords.words(self.language))
-        stop_words.update(['de', 'a', 'e', 'que', 'dir="auto"', 'class="style-scope'
-                           'o', 'em', 'da', 'na', 'por', 'para', 'os', 'dos', 'um', 'com', 'é'])
-        emoji_pattern = re.compile("["
-                                    u"\U0001F600-\U0001F64F"  # emoticons
-                                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                    "]+", flags=re.UNICODE)
-        frase = emoji_pattern.sub(r'', frase) # remove emojis
-        frase = re.sub('span dir=""auto"" class=""style-scope yt-formatted-string"">', frase)
-        frase = re.sub('/span><span dir=""auto"" class=""style-scope yt-formatted-string"">', frase)
-        sentence_tokens = sent_tokenize(frase.lower())
-        filtered_sentence = []
-        if self.translate:
-            sentence_tokens = [GoogleTranslator(source='auto', target=self.language).translate(sentence) for sentence in sentence_tokens]
-            for sentence in sentence_tokens:
-                word_tokens = word_tokenize(sentence)
-                filtered_sentence.extend([word for word in word_tokens if word.isalnum() and word not in stop_words])
-            return ' '.join(filtered_sentence)
 
+        stop_words = set(stopwords.words(self.language))
+        word_tokens = word_tokenize(frase.lower())
+        filtered_sentence = [word for word in word_tokens if word.isalnum() and word not in stop_words]
+        return ' '.join(filtered_sentence)
+    
     def sentimento(self, frase=''):
-        '''
-        Retorna um decimal -1 a 1 
-        
-                Parameters:
-                        frase (str): Frase a ser analisada
-        
-                Returns:
-                        result (str): Binary string of the sum of a and b
-        '''
-        preprocessed_comment = self.pre_processamento(frase)
-        sentence_tokens = sent_tokenize(preprocessed_comment)
-        if len(sentence_tokens) == 0:
-            return 0
-        sentiment_scores = [self.sia.polarity_scores(sentence)['compound'] for sentence in sentence_tokens]
-        return sum(sentiment_scores)/len(sentiment_scores)
+        """
+        Calculates the sentiment score of a text using the SentimentIntensityAnalyzer class from the
+        NLTK library.
+
+        Parameters:
+            frase (str): The text to be analyzed.
+
+        Returns:
+            A float between -1 and 1 representing the sentiment score of the text.
+        """
+        preprocessed_sentence = self.pre_processamento(frase)
+        sentiment_score = self.sia.polarity_scores(preprocessed_sentence)
+        return sentiment_score['compound']
+
 
 if __name__ == '__main__':
-    df = pd.read_csv('output.csv')
-    comment_cols = [col for col in df.columns if col.startswith('comments')]
-    cols = comment_cols
-    comments_df = df[cols]
-    comments_df = df.melt(value_vars=comment_cols, value_name='comment')
-    comments_df = comments_df.dropna()
-    comments_df = comments_df.reset_index(drop=True)
-
-    stop_words = set(stopwords.words('portuguese'))
-    stop_words.update(['de', 'a', 'e', 'que', 'dir="auto"', 'class="style-scope'
-                           'o', 'em', 'da', 'na', 'por', 'para', 'os', 'dos', 'um', 'com', 'é'])
-
-    words = []
-    for comment in comments_df['comment']:
-        words += [word.lower() for word in comment.split() if word.lower() not in stop_words]
-
-    word_counts = Counter(words)
-
-    print("Palavras mais comuns:")
-    for word, count in word_counts.most_common(20):
-        print(f"{word}: {count}")
-
-    comments_df['sentiment_score'] = comments_df.apply(lambda x: sentimental_text(language='english', translate=True).sentimento(x['comment']), axis=1)
-    df.to_csv('outputter.csv', index=True)
-    print(comments_df.head())
-
-
+    df = pd.read_csv('amazon.csv')
+    df['text'] = df['reviewTitle'] + ' ' + df['reviewDescription']
+    df['sentiment_score'] = df['text'].apply(lambda x: SentimentalText(language='english', translate=True).sentimento(x))
+    df.to_csv('comments_with_sentiment_scores.csv', index=False)
+    print(df)
